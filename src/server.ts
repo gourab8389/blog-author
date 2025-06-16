1db9a851a43574a29b78510bd7f723fa8f343faa
gourab8389 <deyg6988@gmail.com>
2025-06-17 00:57:05 +0530

    Add database initialization and utility functions for blog management

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { initDB } from "./schema/author.js";
 
 dotenv.config();
 
@@ -7,6 +8,10 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const connectDB = initDB();
+
+connectDB.then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
