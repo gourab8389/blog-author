fc4e56cae1202671e1aa1427b2fbfac2525f32db
gourab8389 <deyg6988@gmail.com>
2025-06-17 18:08:08 +0530

    Fix router initialization in blog routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -3,7 +3,7 @@ import { isAuth } from '../middleware/isAuth.js';
 import { uploadFile } from '../middleware/multer.js';
 import { createBlog, deleteBlog, updateBlog } from '../controllers/blog.js';
 
-const router = express();
+const router = express.Router();
 
 router.post("/blog/new", isAuth, uploadFile, createBlog);
 router.post("/blog/:id", isAuth, uploadFile, updateBlog);
